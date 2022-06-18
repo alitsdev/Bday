0c2e42e7eae32ce29b0f93d9647a5af3b23660f5
Xavi F <xavifortuny@Air-de-Xavi.lan>
2022-06-18 09:22:23 +0200

    problem with rough

--- client/src/services/server-client.ts
+++ client/src/services/server-client.ts
@@ -1,66 +1,66 @@
 const BASE_URL = 'http://localhost:3001';
 
 type template = {
-  host: string;
-  strickers: any[];
-  name: string;
-  age: number;
-  date: Date;
-  time: string;
-  address: string;
+	host: string;
+	strickers: any[];
+	name: string;
+	age: number;
+	date: Date;
+	time: string;
+	address: string;
 };
 type guest = {
-  name: string;
-  mail: string;
-  host: string;
-  invitationSent: boolean;
-  confirmerd: boolean;
+	name: string;
+	mail: string;
+	host: string;
+	invitationSent: boolean;
+	confirmerd: boolean;
 };
 type user = {
-  userId: string;
-  name: string;
-  email: string;
-  password: string;
+	userId: string;
+	name: string;
+	email: string;
+	password: string;
 };
 
-export const getTemplate = async (userId = 'alicia') => {
-  await getRequest(`${userId}/template`);
+export const getTemplate = async (userId = '11') => {
+	await getRequest(`${userId}/template`);
 };
 export const getGuests = async (userId: string) => {
-  await getRequest(`${userId}/guests`);
+	await getRequest(`${userId}/guests`);
 };
 export const getUser = (userId: string) => {
-  getRequest(`${userId}`);
+	getRequest(`${userId}`);
 };
 
 export const postTemplate = async (userId: string, template: template) => {
-  await postRequest(`${userId}/template`, template);
+	await postRequest(`${userId}/template`, template);
 };
 export const postGuest = async (userId: string, guest: guest) => {
-  await postRequest(`${userId}/guests`, guest);
+	await postRequest(`${userId}/guests`, guest);
 };
 export const postUser = (newUser: user) => {
-  postRequest('register', newUser);
+	postRequest('register', newUser);
 };
 
 const getRequest = async (url: string) => {
-  try {
-    const result = await fetch(`${BASE_URL}/${url}`);
-    return await result.json();
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/${url}`);
+		return await result.json();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const postRequest = async (url: string, item: guest | user | template) => {
-  try {
-    const result = await fetch(`${BASE_URL}/${url}`, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(item),
-    });
-    return await result.json();
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/${url}`, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(item),
+		});
+		return await result.json();
+	} catch (error) {
+		console.log(error);
+	}
 };
