7719d8774034c52cbbcd4aa00ef700c496ed0c3b
Xavi F <xavifortuny@Air-de-Xavi.lan>
2022-06-20 16:01:38 +0200

    keep testing

---
+++ cypress/e2e/frontend.cy.ts
@@ -0,0 +1,133 @@
+describe('homepage', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:3000');
+		cy.get('[name="userId"]').type('hello');
+		cy.get('[name="password"]').type('hello');
+		cy.get('button').click();
+		cy.url().should('include', '/hello/template');
+	});
+});
+
+describe('main functionality', () => {
+	it('checkbox', () => {
+		cy.get(':nth-child(1) > label').click();
+		cy.get(':nth-child(2) > label').click();
+		cy.get(':nth-child(3) > label').click();
+		cy.get(':nth-child(4) > label').click();
+		cy.get(':nth-child(5) > label').click();
+		cy.get(':nth-child(6) > label').click();
+		cy.get(':nth-child(7) > label').click();
+		cy.get(':nth-child(8) > label').click();
+		cy.get(':nth-child(9) > label').click();
+	});
+});
+
+describe('drag and drop containers', () => {
+	it('drag and drop', () => {
+		cy.get('.tool-menu-container')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 200, -200, { force: true })
+			.trigger('mouseup');
+
+		cy.get(':nth-child(5) > label').click();
+		cy.get('.color-menu > div')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 200, 100, { force: true });
+
+		cy.get(':nth-child(8) > label').click();
+		cy.get('#details-form')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 200, -100, { force: true });
+
+		cy.get(':nth-child(9) > label').click();
+		cy.get('.text-menu-container')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 200, 300, { force: true });
+	});
+});
+
+describe('draw, paint and delete shapes', () => {
+	it('a line', () => {
+		cy.get('.tool-menu > :nth-child(1)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 0, 0)
+			.trigger('mousemove', 200, 150, { force: true })
+			.trigger('mouseup');
+		cy.get('.tool-menu > :nth-child(5)').click();
+		cy.get('#red').click();
+		cy.get('#canvas').click(200, 150);
+		cy.get('.tool-menu > :nth-child(7)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mouseup');
+	});
+
+	it('a triangle', () => {
+		cy.get('.tool-menu > :nth-child(2)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 0, 0)
+			.trigger('mousemove', 50, 50, { force: true })
+			.trigger('mouseup');
+		cy.get('.tool-menu > :nth-child(5)').click();
+		cy.get('#blue').click();
+		cy.get('#canvas').click(50, 50);
+		cy.get('.tool-menu > :nth-child(7)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mouseup');
+	});
+
+	it('a rectangle', () => {
+		cy.get('.tool-menu > :nth-child(3)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 0, 0)
+			.trigger('mousemove', 200, 150, { force: true })
+			.trigger('mouseup');
+		cy.get('.tool-menu > :nth-child(5)').click();
+		cy.get('#green').click();
+		cy.get('#canvas').click(200, 150);
+		cy.get('.tool-menu > :nth-child(7)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mouseup');
+	});
+
+	it('a circle', () => {
+		cy.get('.tool-menu > :nth-child(4)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mousemove', 0, 0)
+			.trigger('mousemove', 200, 200, { force: true })
+			.trigger('mouseup');
+		cy.get('.tool-menu > :nth-child(5)').click();
+		cy.get('#yellow').click();
+		cy.get('#canvas').click(200, 200);
+		cy.get('.tool-menu > :nth-child(7)').click();
+		cy.get('#canvas')
+			.trigger('mousedown', { which: 0, force: true })
+			.trigger('mouseup');
+	});
+
+	describe('filling forms', () => {
+		it('text-form', () => {
+			cy.get('.tool-menu > :nth-child(8)').click();
+			cy.get('#details-form');
+			cy.get('[name="name"]').type('JoanMarc');
+			cy.get('[type="number"]').type('30');
+			cy.get('[type="date"]').type('2022-03-12');
+			cy.get('[type="time"]').type('15:00');
+			cy.get('[name="address"]').type('C/cardedeu, 12');
+			cy.get('#details-form > button').click();
+		});
+
+		it('guest-list', () => {
+			cy.get('.tool-menu > :nth-child(9)').click();
+			cy.get('.text-menu-container');
+			cy.get('[type="text"]').type('Xavi');
+			cy.get('[type="email"]').type('hola@gmail.com');
+		});
+	});
+});
--- cypress/e2e/homepage.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('homepage', () => {
-	it('passes', () => {
-		cy.visit('http://localhost:3000');
-		cy.get('[name="userId"]').type('hello');
-		cy.get('[name="password"]').type('hello');
-		cy.get('button').click();
-	});
-});
