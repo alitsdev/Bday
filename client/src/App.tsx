4377633141f9e5cb71fcebfe38d6e191917cb9f7
mortyrise <joanmarcdomenech@gmail.com>
2022-06-21 20:26:11 +0200

    implemented, usersID from google

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,25 +6,30 @@ import Editor from './Pages/Editor';
 import PartyLandingPage from './Pages/Party-landing-page';
 
 type UserId = string;
+type UserMail = string;
 
 const App = () => {
-	const [userId, setUserId] = React.useState<UserId>('alicia');
+  const [userId, setUserId] = React.useState<UserId>('');
+  const [userMail, setUserMail] = React.useState<UserMail>('');
 
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Login setUserId={setUserId} />} />
-				<Route
-					path={`/:${userId}/template`}
-					element={<Editor userId={userId} />}
-				/>
-				<Route
-					path={`/:${userId}/invitation`}
-					element={<PartyLandingPage userId={userId} />}
-				/>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Login setUserId={setUserId} setUserMail={setUserMail} />}
+        />
+        <Route
+          path={`/:${userId}/template`}
+          element={<Editor userId={userId} userMail={userMail} />}
+        />
+        <Route
+          path={`/:${userId}/invitation`}
+          element={<PartyLandingPage userId={userId} userMail={userMail} />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -6,51 +6,54 @@ import { auth } from '../services/firebase';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 type LoginProps = {
-	setUserId: React.Dispatch<React.SetStateAction<string>>;
+  setUserId: React.Dispatch<React.SetStateAction<string>>;
+  setUserMail: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const Login: React.FC<LoginProps> = ({ setUserId }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const onSubmitHandler = (event: React.FormEvent) => {
-		event.preventDefault();
+  const onSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
 
-		const target = event.target as typeof event.target & {
-			userId: { value: string };
-		};
+    const target = event.target as typeof event.target & {
+      userId: { value: string };
+    };
 
-		const userId: string = target.userId.value;
-		if (userId) {
-			setUserId(userId);
-		}
-		navigate(`/${userId}/template`);
-	};
+    const userId: string = target.userId.value;
+    if (userId) {
+      setUserId(userId);
+    }
+    navigate(`/${userId}/template`);
+  };
 
-	const signInWithGoogle = () => {
-		const provider = new GoogleAuthProvider();
-		signInWithPopup(auth, provider)
-			.then((result) => {
-				const userId = result.user.displayName;
-				navigate(`/${userId}/template`);
-				console.log(result);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const googleUserId = result.user.uid;
+        setUserId(googleUserId);
+        navigate(`/${googleUserId}/template`);
+        console.log(result);
+        //TODO - postUser with details added from the google login ( result.user.)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	return (
-		<div>
-			<div id="bday">Bday.</div>
-			<form className="login-form" onSubmit={onSubmitHandler}>
-				<label>User Id</label>
-				<input type="text" name="userId" placeholder="user Id" />
-				<label>Password</label>
-				<input type="text" name="password" placeholder="password" />
-				<button type="submit">Login</button>
-			</form>
-			<GoogleButton className="googleBtn" onClick={signInWithGoogle} />
-		</div>
-	);
+  return (
+    <div>
+      <div id="bday">Bday.</div>
+      <form className="login-form" onSubmit={onSubmitHandler}>
+        <label>User Id</label>
+        <input type="text" name="userId" placeholder="user Id" />
+        <label>Password</label>
+        <input type="text" name="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+      <GoogleButton className="googleBtn" onClick={signInWithGoogle} />
+    </div>
+  );
 };
 export default Login;
