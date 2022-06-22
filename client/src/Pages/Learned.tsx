1f956af3534e993b1a4aee25b5f3045cfef4e719
Xavi F <xavifortuny@Air-de-Xavi.lan>
2022-06-22 09:51:43 +0200

    presentation

---
+++ client/src/Pages/Learned.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Learned() {
+	return (
+		<>
+			<h1>Why did we choose this project?</h1>
+			<ul>
+				<p>1</p>
+				<p>1</p>
+				<p>1</p>
+				<p>1</p>
+			</ul>
+
+			<h1>What did we do?</h1>
+			<ul>
+				<p>1</p>
+				<p>2</p>
+				<p>1</p>
+				<p>1</p>
+			</ul>
+
+			<h1>Lessons learned</h1>
+			<ul>
+				<p>1</p>
+				<p>2</p>
+				<p>1</p>
+				<p>1</p>
+			</ul>
+		</>
+	);
+}
+
+export default Learned;
